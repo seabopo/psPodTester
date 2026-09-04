5ae1955572af991f718858aa10e162154a65a1cd
seabopo <seabopo@icloud.com>
2026-09-04 01:53:11 -0500

    fix arm cpu test.

--- psPodTester/private/environment/Test-IsArmCpu.ps1
+++ psPodTester/private/environment/Test-IsArmCpu.ps1
@@ -1,7 +1,7 @@
 function Test-IsArmCPU {
     <#
     .DESCRIPTION
-        Determins if the CPU is an ARM CPU. This is only used on the container host to determing the image to use.
+        Determines if the CPU is an ARM CPU.
     #>
 
     [CmdletBinding()]
@@ -9,25 +9,13 @@ function Test-IsArmCPU {
     param ( )
 
     $isArm = $false
-
-    if ( $env:PSPOD_ENV_IsArmCPU ) {
-        $isArm = [bool] [int] $env:PSPOD_ENV_IsArmCPU
+    if ($isWindows) {
+        $isArm = ($ENV:PROCESSOR_ARCHITECTURE -like '*ARM*')
     }
     else {
-        if ( -not (Test-IsContainer) ) {
-            if ($IsWindows) {
-                $isArm = ($ENV:PROCESSOR_ARCHITECTURE -like '*ARM*')
-            }
-            elseif ( $isMacOS ) {
-                $isArm = ((uname -m) -match 'arm|arm64|aarch64')
-                # hw.optional.arm64: 1
-            }
-            elseif ( $isLinux ) {
-                $isArm = ((uname -m) -match 'arm|arm64|aarch64')
-            }
-        }
-        $env:PSPOD_ENV_IsArmCPU = ( $isArm ? 1 : 0 )
+        $isArm = ((uname -m) -match 'arm|arm64|aarch64')
     }
+    $env:PSPOD_ENV_IsArmCPU = ( $isArm ? 1 : 0 )
 
     return $isArm
 }
